883e1e54172d006420bc6e5f876d0400df900dcf
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-12-01 14:01:54 +0100

    add script

---
+++ js/script.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+
+        $("button#green").click(function() {
+            $("body").removeClass();
+            $("body").addClass("green-background");
+          });
+        
+          $("button#blue").click(function() {
+            $("body").removeClass();
+            $("body").addClass("blue-background");
+          });
+        
+          $("button#white").click(function() {
+            $("body").removeClass();
+            $("body").addClass("white-background");
+          });
+    
+    
+          $('#targetParagraph').click(function () {
+              $(this).removeClass();
+              $(this).addClass('highlight');
+            
+        });
+    
+       
+        $('#rem').click(function () {
+            $('.tee').show().toggle();
+        });
+    
+        $('#ab1').click(function () {
+        $('.ab').show().toggle();
+        });
+    
+        $('#cd2').click(function () {
+        $('.cd').show().toggle();
+        });
+    
+        $('#ef3').click(function () {
+        $('.ef').show().toggle();
+        });
+    
+        $('#gh4').click(function () {
+        $('.gh').show().toggle();
+        });
+    
+        $('#ij5').click(function () {
+        $('.ij').show().toggle();
+        });
+    
+        $('#kl6').click(function () {
+        $('.kl').show().toggle();
+        });
+    
+        $('#mn7').click(function () {
+        $('.mn').show().toggle();
+        });
+    
+        $('#op8').click(function () {
+        $('.op').show().toggle();
+        });
+    
+        $('#qr9').click(function () {
+        $('.qr').show().toggle();
+        });
+    
+        $('#st1').click(function () {
+        $('.st').show().toggle();
+        });
+    
+        $('#uv2').click(function () {
+        $('.uv').show().toggle();
+        });
+    
+        $('#wx3').click(function () {
+        $('.wx').show().toggle();
+        });
+    
+        $('#yz4').click(function () {
+        $('.yz').show().toggle();
+        });
+    
+        $('#125').click(function () {
+        $('.12').show().toggle();
+        });
+    
+        $('#346').click(function () {
+        $('.34').show().toggle();
+        });
+    
+        $('#567').click(function () {
+        $('.56').show().toggle();
+        });
+    
+        $('#788').click(function () {
+        $('.78').show().toggle();
+        });
+    
+        
+        $('#909').click(function () {
+            $('.90').show().toggle();
+            });
+        
+    
+    });
